fb16d6aabc520432e25c5fa12570ad586c501975
Anyan Zhao <AZhao4@slb.com>
2024-11-06 18:54:02 +0800

    feat: add chat box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatBoxComponent } from './components/chat-box/chat-box.component';
 import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatInputModule } from '@angular/material/input'; 
+import { MatButtonModule, MatIconButton } from '@angular/material/button'; 
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,9 +20,16 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatInputModule, 
+    MatButtonModule, 
+    MatCardModule,
+    MatIconModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/chat-box/chat-box.component.ts
+++ src/app/components/chat-box/chat-box.component.ts
@@ -7,14 +7,24 @@ import { AIGenerationService } from '../../services/text-generation-response.ser
   styleUrl: './chat-box.component.css'
 })
 export class ChatBoxComponent {    
-  public prompt: string = "我今天晚上吃什么呢？！"
+  public prompt: string = ""
   public response: string = "";
+  responseIcon: string[] = ['mood', 'mood_bad', 'sentiment_satisfied_alt', 'sentiment_neutral', 'sentiment_very_satisfied'];
+
+  iconSelection = 0;
 
   constructor(public service: AIGenerationService) { 
-    this.service.generateContent(this.prompt).subscribe(
-      response => { 
-        this.response = response;
-      });
+    // this.service.generateContent(this.prompt).subscribe(
+    //   response => { 
+    //     this.response = response;
+    //   });
+    this.response = "mock response";
+  }
+
+  onSubmit()
+  {
+    this.iconSelection = (this.iconSelection + 1) % this.responseIcon.length;
+    this.response = "new response";
   }
 
 }
